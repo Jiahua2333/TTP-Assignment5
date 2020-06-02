a4fa11f963ed079540b68c9fd66a11a59b449b3f
Jiahua Liao <ljh15088131916@gmail.com>
2020-06-01 22:20:59 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,22 @@
+// const addRow = document.getElementById('addRow');
+// const removeRow = document.getElementById('removeRow');
+// const addColumn = document.getElementById('addColumn1');
+// const removeColumn = document.getElementById('removeColumn');
+// const fillAllUncolored = document.getElementById('fillAllUncolored');
+// const fillAll = document.getElementById('fillAll');
+// const clearAll = document.getElementById('clearAll');
+// const table = document.getElementById("table");
+
+// let cols = 5, rows = 0;
+
+// addRow.addEventListener('click', (event) => {
+//     let row = document.createElement("tr");
+//     row.classList.add("row" + rows + 1);
+//     table.appendChild(row);
+//     rows++;
+//     for(let i = 0; i < cols; i++) {
+//         let cell = document.createElement("td");
+//         cell.classList.add("cell" + i + 1);
+//         row.appendChild(cell);
+//     }
+// });
